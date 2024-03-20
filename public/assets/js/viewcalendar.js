28b8c1b4a6ce5bd7774ad39d9dc907381693d76d
Riansryn <riansuryana056@gmail.com>
2024-03-21 00:48:33 +0700

    Update calender By Rian

--- public/assets/js/viewcalendar.js
+++ public/assets/js/viewcalendar.js
@@ -6,8 +6,17 @@ $(document).ready(function() {
             return {
                 title: 'Melakukan Schedule Visit - ' + visit.pic,
                 start: visit.schedule,
-                end: visit.due_date
+                end: visit.due_date,
+                description: visit.description // tambahkan deskripsi jika diperlukan
             };
-        })
+        }),
+        eventClick: function(calEvent, jsEvent, view) {
+            $('#eventModal #eventDetails').html(
+                '<p><strong>' + calEvent.title + '</strong></p>' +
+                '<p><strong>Tanggal:</strong> ' + calEvent.start.format('YYYY-MM-DD') + '</p>' +
+                '<p><strong>Deskripsi:</strong> ' + (calEvent.description ? calEvent.description : 'Tidak ada deskripsi') + '</p>'
+            );
+            $('#eventModal').modal('show');
+        }
     });
 });
