7a04632c541ed1e2b7d986a14a28a818a7a6ead0
MULA IMMANUEL <0320210044@polman.astra.ac.id>
2024-04-03 08:22:52 +0700

    Update By Mula

--- resources/views/dashboard/dashboardHandling.blade.php
+++ resources/views/dashboard/dashboardHandling.blade.php
@@ -582,7 +582,7 @@
 
             // Inisialisasi array untuk bulan-bulan
             var months = [];
-            for (var i = 1; i <= 12; i++) {
+            for (var i = 0; i <= 12; i++) {
                 months.push(getMonthName(i));
             }
 
